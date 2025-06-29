cfa46080b58a93f14d64fd2e2321bbc08cce6a96
ryoup <ryou.2000.0929@gmail.com>
2025-06-30 04:01:13 +0900

    a

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-let userThreshold = 0.45; // デフォルトのしきい値
+let userThreshold = 0.60; // デフォルトのしきい値
 
 // ボタンを押すと閾値を切り替える
 function toggleThreshold() {
     const button = document.getElementById("thresholdButton");
-    if (userThreshold === 0.50) {
+    if (userThreshold === 0.60) {
         userThreshold = 0.35;
         button.innerText = "通常に戻す";
         document.getElementById("thresholdDisplay").innerText = "甘め";
     } else {
-        userThreshold = 0.50;
+        userThreshold = 0.60;
         button.innerText = "甘めにする";
         document.getElementById("thresholdDisplay").innerText = "通常";
     }
@@ -149,7 +149,7 @@ function drawCircle(ctx, x, y) {
     ctx.strokeStyle = "black"; // 円の色
     ctx.lineWidth = 10; // 線の太さ
     ctx.beginPath();
-    ctx.arc(x, y, 24, 0, 2 * Math.PI); // 半径10pxの円
+    ctx.arc(x, y, 30, 0, 2 * Math.PI); // 半径10pxの円
     ctx.stroke();
 }
 
